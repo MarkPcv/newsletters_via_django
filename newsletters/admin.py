e6cbf6fbdd621621b69a98d113aba1c651b43073
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-07-23 12:01:14 +0300

    Create superuser and modify admin page

--- newsletters/admin.py
+++ newsletters/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from newsletters.models import Client, Content, Newsletter, Trial
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'fullname',)
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'message',)
+    search_fields = ('title', 'message',)
+
+
+@admin.register(Trial)
+class TrialAdmin(admin.ModelAdmin):
+    list_display = ('date', 'status',)
+    ordering = ('date',)
+    list_filter = ('content',)
