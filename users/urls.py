194d295314f8ad2e287684e32a261cc419ed851b
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-07-30 10:51:45 +0300

    User logout implementation

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import LoginView
+from users.views import LoginView, LogoutView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
-#     path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 #     path('register/', RegisterView.as_view(), name='register'),
 #     path('resetpwd/', reset, name='reset'),
 #     path('profile/', UserUpdateView.as_view(), name='profile'),
--- users/views.py
+++ users/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView as BaseLoginView
+from django.contrib.auth.views import LogoutView as BaseLogoutView
 
 class LoginView(BaseLoginView):
     template_name = 'users/login.html'
+
+
+class LogoutView(BaseLogoutView):
+    pass
