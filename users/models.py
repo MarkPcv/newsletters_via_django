a1e89759b1bc44ad1a8e20e480fd939350da79a7
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-07-30 09:27:37 +0300

    Update models.py for users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from newsletters.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    phone = models.CharField(max_length=35, verbose_name='номер телефона',
+                             **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
